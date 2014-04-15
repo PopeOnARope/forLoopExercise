76241a316969599399e652509b800e555879854e
popeonarope <johnvepope@gmail.com>
2014-04-15 00:48:17 -0400

    added a whole lotta shit

--- script.js
+++ script.js
@@ -1,16 +1,55 @@
-// $(document).ready(function(){
+$(document).ready(function() {
 
-var menu = ["Home", "About", "Contact", "Blog"];
+	var menuArray = ["home", "about", "blog", "contact"];
 
-var nav = function(x) {
-	var markup = "";
-	var i;
+	var menuFunk = function(data){
+		
 
-	for(i = 0; i < x.length; i++) {
-		item = "<li>" + x[i] + "</i>";
-		console.log(item);
+		for(i=0; i<data.length; i++) {
+			$('.nav').append('<li><a href =' +'"' + data[i] + '.html" </a>' + data[i] + '</li>');
+			
+		}
 	}
-}
-nav(menu);
 
-// });
+
+	menuFunk(menuArray);
+
+	var picGen = function(){
+
+		for(i=1; i<=6; i++) {
+			$('.images').append('<img src="img/aerial0' + i + '.jpg">');
+		}
+	}
+	picGen();
+
+	// var gradient = function(){
+	// 	for(i=0; i<9; i++);
+	// 		$('.container').append('<style')
+	// }
+
+	var nameArray = ['jim', 'john', 'jake', 'jingleheimer', 'schmidt'];
+
+	var squares = function(){
+		for(i=1000; i>2; i=i/2) {
+			$('.container2').append('<div style = "width:' + i + 'px"</div>');
+
+
+		}
+	}
+	squares();
+
+	var curve = function() {
+		for(i=1; i<255; i=i+3) {
+			$('.container3').append('<div style = "width:' + i + 'px; background-color: rgba(' + i + ',' + i + ',' + i + ',1);" </div>');
+		}
+	}
+	curve();
+
+
+	var circle = function() {
+		for(i=1; i<100; i=i+3) {
+			$('.container4').append('<div style = "border-radius:' + i + 'px;" </div>');
+		}
+	}
+	circle();
+});
